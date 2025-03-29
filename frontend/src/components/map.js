df650c1ec884d3a380285b1f4d207670f4883a0b
kevinli5371 <kevinli5371@gmail.com>
2025-03-29 14:35:08 -0400

    nothing lol

---
+++ frontend/src/components/map.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+function Map() {
+    const mapContainerRef = useRef(null);
+    // useEffect(() => {
+    //     mapboxgl
+    // })
+}
