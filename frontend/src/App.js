6254896bc1070b7f76f7e6b82bfc1c6fc15ef144
kevinli5371 <kevinli5371@gmail.com>
2025-03-29 17:35:53 -0400

    formatted sidebar with logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Map from './components/map';
+import TapInLogoWhite from './assets/TapInLogoWhite.png';
 
 function App() {
   return (
-    <div className = "App">
+    <div className = "app">
+      <div className = "sidebar">
+        <img src={TapInLogoWhite} alt='TapInLogo' height='67px' width='199px' style={ {opacity: 1.43} }/>
+      </div>
       <Map />
     </div>
   );
